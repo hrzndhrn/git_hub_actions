a9f54f034b9e6cfcf8b4d71702f247408d747b4f
Marcus Kruse <esurk.sucram@gmail.com>
2025-04-17 19:20:11 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule GitHubActions.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/hrzndhrn/git_hub_actions"
-  @version "0.3.3"
+  @version "0.3.4"
 
   def project do
     [
